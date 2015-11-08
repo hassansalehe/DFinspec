dde45f3ed9ff0269b00bcbe3ef19f35128727e13
Hassan Salehe Matar <hmatar@ku.edu.tr>
2015-11-08 07:25:48 +0200

    Callbacks renaming / rearrangeing

--- instrumentor/Callbacks.cpp
+++ instrumentor/Callbacks.cpp
@@ -1,49 +1,20 @@
 #include <iostream>
+#include "Callbacks.h"
+#include "Logger.h"
+
 using namespace std;
-
-extern "C" {
-
-// callbacks at creation of task
-void AdfCreateTask(void **intokens, void* fn);
-
-// callbacks for tokens
-void regInToken(void * tokenAddr, unsigned long size);
-void regOutToken(void * bufferAddr, void * tokenAddr, unsigned long size);
-
-// callbacks for memory access, race detection
-void AdfMemRead8(void *addr);
-void AdfMemRead4(void *addr);
-void AdfMemRead1(void *addr);
-void AdfMemWrite8(void *addr, void * value);
-void AdfMemWrite4(void *addr, void * value);
-void AdfMemWrite1(void *addr, void * value);
-
-// task begin and end callbacks
-void taskStartFunc(void* addr);
-void taskFinishFunc(void* addr);
-
-void toolVptrUpdate(void *addr, void * value);
-void toolVptrLoad(void *addr, void * value);
-
-void hassanFun(void * addr, int value);
-};
-
-void AdfCreateTask(void**intokens, void* fn) {
-//   if(intokens)
-//     cout << "IN-TOKEN: " << *intokens << " Function: "<< fn<<  endl;
-}
 
 long int getTokenValue(void *addr, unsigned long size)
 {
   if( size == sizeof(char) )
      return *(static_cast<char *>(addr));
-  
+
   if( size == sizeof(short) )
      return *(static_cast<short *>(addr));
 
   if( size == sizeof(int) )
      return *(static_cast<int *>(addr));
-  
+
   if( size == sizeof(long) )
      return *(static_cast<long *>(addr));
 
@@ -51,18 +22,37 @@ long int getTokenValue(void *addr, unsigned long size)
      return *(static_cast<long long *>(addr));
    return -1;
 }
+  // to initialize the logger
+  void INS_Init() {
+    INS::Init();
+  }
+
+  // to finalize na book-keep the logger
+  void INS_Fini() {
+    INS::Finalize();
+  }
 
 
-void regInToken(void * tokenAddr, unsigned long size)
-{
-   long int token = getTokenValue(tokenAddr, size);
-   cout << "InToken: " << token << " addr: " << tokenAddr << endl;
+void AdfCreateTask(void**intokens, void* fn) {
+//   if(intokens)
+//     cout << "IN-TOKEN: " << *intokens << " Function: "<< fn<<  endl;
 }
 
-void regOutToken(void * bufferAddr, void * tokenAddr, unsigned long size)
+void INS_RegInToken(void * tokenAddr, unsigned long size)
 {
-   long int token = getTokenValue(tokenAddr, size);
-   cout << "OutToken: " << token << " addr: " << bufferAddr << endl;
+   INTEGER taskID = 0;
+   INTEGER value = getTokenValue(tokenAddr, size);
+   STRING taskName = "A";
+   INS::TaskStartLog(taskID, tokenAddr, value, taskName);
+   cout << "InToken: " << value << " addr: " << tokenAddr << endl;
+}
+
+void INS_RegOutToken(ADDRESS bufLocAddr, ADDRESS tokenAddr, unsigned long size)
+{
+  INTEGER taskID = 0;
+  INTEGER value = getTokenValue(tokenAddr, size);
+  INS::TaskEndLog(taskID, bufLocAddr, value);
+  cout << "OutToken: " << value << " addr: " << bufLocAddr << endl;
 }
 
 
@@ -70,12 +60,12 @@ void hassanFun(void * addr, int value) {
   cout << "HASSAN FUNC addr: " << addr << " value_from_addr: "<< *(static_cast<int *>(addr))<< " value: "<< value << endl;
 }
 
-void taskStartFunc(void* addr) {
+void INS_TaskStartFunc(void* addr) {
   cout << "Task_Started, (return address : " << addr <<")"<< endl;
 }
 
 
-void taskFinishFunc(void* addr) {
+void INS_TaskFinishFunc(void* addr) {
   cout << "Task_Ended: (" << addr << ")"<< endl;
 }
 
@@ -87,27 +77,53 @@ void toolVptrLoad(void *addr, void * value) {
   cout << " VPTR read: addr:" << addr << " value " << (long int)value << endl;
 }
 
-void AdfMemRead1(void *addr) {
-    cout << "read:  addr1: "<< addr<< endl;
+void INS_AdfMemRead1(void *addr) {
+
+  INTEGER taskID = 0;
+  INTEGER value = getTokenValue(addr, 1);
+  INS::Read(taskID, addr, value);
+
+  cout << "read:  addr1: "<< addr<< endl;
 }
 
-void AdfMemRead4(void *addr) {
-    cout << "read:  addr4: "<< addr<< endl;
+void INS_AdfMemRead4(void *addr) {
+  cout << "read:  addr4: "<< addr<< endl;
+
+  INTEGER taskID = 1;
+  INTEGER value = getTokenValue(addr, 4);
+  INS::Read(taskID, addr, value);
+    cout << "read:  addr4: "<< addr<< " value: " << value << endl;
 }
 
-void AdfMemRead8(void *addr) {
-    cout << "read:  addr8: "<< addr<< endl;
+void INS_AdfMemRead8(void *addr) {
+
+  INTEGER taskID = 0;
+  INTEGER value = getTokenValue(addr, 8);
+  INS::Read(taskID, addr, value);
+
+  cout << "read:  addr8: "<< addr << " value: " << value << endl;
 }
 
-void AdfMemWrite4(void *addr, void * value) {
-    cout << "write: addr4:" << addr << " value " << (long int)value << endl;
+void INS_AdfMemWrite1(void *addr, void * value) {
+
+  INTEGER taskID = 0;
+  INTEGER value_ = getTokenValue(addr, 1);
+  cout << "write: addr1:" << addr << " value " << (long int)value << endl;
+  INS::Write(taskID, addr, (long int)value);
 }
 
-void AdfMemWrite8(void *addr, void * value) {
-    cout << "write: addr8:" << addr << " value " << (long int)value << endl;
+void INS_AdfMemWrite4(void *addr, void * value) {
+
+  INTEGER taskID = 0;
+  cout << "write: addr4:" << addr << " value " << (long int)value << endl;
+  INS::Write(taskID, addr, (long int)value);
 }
 
-void AdfMemWrite1(void *addr, void * value) {
-    cout << "write: addr1:" << addr << " value " << (long int)value << endl;
+void INS_AdfMemWrite8(void *addr, void * value) {
+
+  INTEGER taskID = 0;
+  INTEGER value_ = getTokenValue(addr, 8);
+  cout << "write: addr8:" << addr << " value " << (long int)value << endl;
+  INS::Write(taskID, addr, (long int)value);
 }
 
