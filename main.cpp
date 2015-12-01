7230027e28b9a1e2a33c125f6b1dbe6f42f596e5
Hassan Salehe Matar XEON <hmatar@ku.edu.tr>
2015-12-01 15:12:38 +0200

    Measure execution time of non-determinism checking

--- main.cpp
+++ main.cpp
@@ -22,9 +22,11 @@
 #include <cctype>
 #include <vector>
 #include <set>
+#include <chrono>
 
 #include "checker.h"  // header
 
+using namespace std::chrono;
 
 int main(int argc, char * argv[])
 {
@@ -36,6 +38,9 @@ int main(int argc, char * argv[])
     cout << endl;
     exit(-1);
   }
+
+  // take time at begin of analysis
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
   Checker aChecker; // checker instance
   string logLine;
@@ -51,8 +56,14 @@ int main(int argc, char * argv[])
   while( getline(log, logLine) ) {
     aChecker.processLogLines(logLine);
   }
+
+  // take time at end of analyis
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+  cout << "Checker execution time: "<< duration << " microseconds" << endl;
+
   // testing writes
-  aChecker.testing();
+  //aChecker.testing();
   aChecker.reportConflicts();
   aChecker.printHBGraph();
 
