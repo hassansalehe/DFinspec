d3c965d06e44668ed012325e13563138680215c6
Hassan Salehe Matar XEON <hmatar@ku.edu.tr>
2015-12-14 22:48:49 +0200

    Simplified version of reporting nondeterminism bug

--- checker.cpp
+++ checker.cpp
@@ -249,6 +249,8 @@ VOID Checker::reportConflicts() {
   cout << "                                                            " << endl;
   cout << "        Non-determinism checking report                     " << endl;
   cout << "                                                            " << endl;
+
+#ifdef VERBOSE // print full summary
   cout << " The following " << conflictTable.size() <<" task pairs have conflicts: " << endl;
 
   for(auto it = conflictTable.begin(); it != conflictTable.end(); ++it) {
@@ -268,6 +270,29 @@ VOID Checker::reportConflicts() {
     }
   }
 
+#else
+
+  // a pair of conflicting task body with a set of line numbers
+  map< pair<string,string>,  set<pair<int,int>>> tasksAndLines;
+  for(auto it = conflictTable.begin(); it != conflictTable.end(); ++it) {
+    pair<string, string> namesPair = make_pair(it->second.task1Name, it->second.task2Name);
+    if(tasksAndLines.find(namesPair) == tasksAndLines.end())
+      tasksAndLines[namesPair] = set<pair<int,int>>();
+
+    // get line numbers
+    for(auto conf = it->second.addresses.begin(); conf != it->second.addresses.end(); conf++) {
+      pair<int,int> lines = make_pair(conf->lineNo1, conf->lineNo2);
+      tasksAndLines[namesPair].insert(lines);
+    }
+  }
+  for(auto it = tasksAndLines.begin(); it!= tasksAndLines.end(); it++)
+  {
+    cout << it->first.first << " <--> " << it->first.second << ": line numbers  {";
+    for(auto ot = it->second.begin(); ot != it->second.end(); ot++)
+      cout << ot->first <<" - "<< ot->second << ", ";
+    cout << "}" << endl;
+  }
+#endif
   cout << "                                                            " << endl;
   cout << "============================================================" << endl;
 }
