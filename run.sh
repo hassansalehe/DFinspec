1e2777ea5c9e94ef22d8b521cc39a6da35e33a6f
Hassan Salehe Matar <hassansalehe@gmail.com>
2017-11-18 14:20:09 +0300

    Fix compilation issues due to refactoring

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
+#/////////////////////////////////////////////////////////////////
+#//  DFinspec: a lightweight non-determinism checking
+#//          tool for shared memory DataFlow applications
+#//
+#//    Copyright (c) 2015 - 2017 Hassan Salehe Matar
+#//      Copying or using this code by any means whatsoever
+#//      without consent of the owner is strictly prohibited.
+#//
+#//   Contact: hmatar-at-ku-dot-edu-dot-tr
+#//
+#/////////////////////////////////////////////////////////////////
 #!/bin/bash
-export LLVM_DIR=/usr/bin/llvm-3.8/share/llvm/cmake
 
 HOME=`pwd`
 
 ## Benchmarks-related directories ##
-BENCHS_DIR=$HOME/benchmarks
+BENCHS_DIR=$HOME/src/tests/adf_benchmarks
 SRC=$BENCHS_DIR/src
 INC=$BENCHS_DIR/include
 STMSTL=$BENCHS_DIR/include/stm_stl
@@ -12,33 +22,21 @@ ATOMICOPS=$BENCHS_DIR/include/atomic_ops
 TMMISC=$BENCHS_DIR/tmmisc
 
 # Instrumentation passes directory
-INSTR_DIR=$HOME/passes
+INSTR_DIR=$HOME/src/passes
 BIN_DIR=$HOME/bin
-
+procNo=`cat /proc/cpuinfo | grep processor | wc -l`
 
 ### 1. ###
-echo -e "\033[1;95mDFinspec: Building the ADFinspec tool.\033[m"
+echo -e "\033[1;95mDFinspec: Building detection libraries, and LLVM passes for instrumentations.\033[m"
 ##  ##
 cd $HOME
-make  || { echo 'Building ADFinspec failed' ; exit 1; }
+mkdir -p .build
+cd .build/
+rm -rf *  > /dev/null 2>&1
+cmake .. || { echo 'Building ADFinspec failed' ; exit 1; }
+make -j${procNo} || { echo 'Building ADFinspec failed' ; exit 1; }
 
 ### 2. ###
-mkdir -p .build
-mkdir -p bin
-
-cd .build/
-
-rm -rf *  > /dev/null 2>&1
-
-echo -e "\033[1;95mDFinspec: Building LLVM passes for runtime and application instrumentations.\033[m"
-rm -rf $INSTR_DIR/Makefile $INSTR_DIR/*.so $INSTR_DIR/CMakeFile $INSTR_DIR/*.cmake > /dev/null 2>&1
-cmake $INSTR_DIR
-make
-cp *.so $BIN_DIR/
-g++ -c -O3 -std=c++11 $INSTR_DIR/Logger.cpp -I$INSTR_DIR/includes -I$INSTR_DIR/../common -o $BIN_DIR/Logger.o  || { echo 'Compiling Logger.cpp failed' ; exit 1; }
-g++ -c -O3 -std=c++11 $INSTR_DIR/Callbacks.cpp -I$INSTR_DIR/includes -I$INSTR_DIR/../common -o $BIN_DIR/Callbacks.o  || { echo 'Compiling Callbacks.cpp failed' ; exit 1; }
-
-### 3. ###
 echo -e "\033[1;95mDFinspec: Building the ADF runtime and the dwarf benchmarks.\033[m"
 cd $BENCHS_DIR
 make clean
@@ -46,7 +44,7 @@ rm ${BENCHS_DIR}/obj/adf/adf.o > /dev/null 2>&1
 
 clang++ -Xclang -load -Xclang $BIN_DIR/libADFTokenDetectorPass.so  -c -g -Wall -O0 -Wno-unused-but-set-variable -I${SRC} -I${INC} -I${STMSTL}  -DADF_STM -DADF -std=c++11 -pthread -I${ATOMICOPS} -I${TMMISC} ${BENCHS_DIR}/src/adf.cpp -o ${BENCHS_DIR}/obj/adf/adf.o  || { echo 'Compiling adf.cpp failed' ; exit 1; }
 
-make  || { echo 'make failed' ; exit 1; }
+make || { echo 'make failed' ; exit 1; }
 
 if [ $? -eq 0 ]; then
     echo -e "\033[1;32mDFinspec: Done.\033[m"
@@ -55,4 +53,5 @@ else
 fi
 
 cd $HOME
-
+echo -e "\033[1;32mDeleting temporary object files.\033[m"
+find . -type f -name "*.o" | grep -v "obj/adf/" | xargs rm
